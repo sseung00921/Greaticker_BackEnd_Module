56153a582a8b80b4f9ca63dce88b13b7449a20e3
sseung00921 <sseung00921@naver.com>
2024-09-02 01:52:52 +0900

    logIn logic refactoring

--- src/main/java/com/greaticker/demo/constants/auth/Auth.java
+++ src/main/java/com/greaticker/demo/constants/auth/Auth.java
@@ -2,5 +2,5 @@ package com.greaticker.demo.constants.auth;
 
 public class Auth {
     static public final String GOOGLE_OAUTH2_IOS_CLIENT_ID = "884290819984-s2dkioqcqqk2e9uam6cvkb7rn0t4hnta.apps.googleusercontent.com";
-    static public final String GOOGLE_OAUTH2_ANDROID_CLIENT_ID = "884290819984-35pc1um5t66r3h8tsdt5e50l40qdg42g.apps.googleusercontent.com";
+    static public final String GOOGLE_OAUTH2_WEB_CLIENT_ID = "884290819984-sfa02gqm4movb857v4cv7bggbuq4o0bs.apps.googleusercontent.com";
 }
