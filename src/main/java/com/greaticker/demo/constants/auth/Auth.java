bac2baf933dae3f2e9d47a45d71c8c416263b0a6
sseung00921 <sseung00921@naver.com>
2024-09-10 13:43:22 +0900

    apply cognito to AuthService.#3

--- src/main/java/com/greaticker/demo/constants/auth/Auth.java
+++ src/main/java/com/greaticker/demo/constants/auth/Auth.java
@@ -1,8 +1,6 @@
 package com.greaticker.demo.constants.auth;
 
 public class Auth {
-    static public final String GOOGLE_OAUTH2_IOS_CLIENT_ID = "884290819984-s2dkioqcqqk2e9uam6cvkb7rn0t4hnta.apps.googleusercontent.com";
-    static public final String GOOGLE_OAUTH2_WEB_CLIENT_ID = "884290819984-sfa02gqm4movb857v4cv7bggbuq4o0bs.apps.googleusercontent.com";
     static public final String COGNITO_JWK_URL = "https://cognito-idp.ap-northeast-2.amazonaws.com/ap-northeast-2_zzXeaAdzU/.well-known/jwks.json";
     static public final String COGNITO_ISSUER = "https://cognito-idp.ap-northeast-2.amazonaws.com/ap-northeast-2_zzXeaAdzU";
     static public final String COGNITO_AUDIENCE = "1r9d8cmn36s4t4al7p52d1lvru";
